8c3b155ba2694711649091b9dc12d89b39d4292a
randikaperera96 <randikaperera135@gmail.com>
2020-07-23 20:15:10 +0530

    "Card" demo

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -2,7 +2,10 @@ import React, { Component } from "react";
 
 class Hero extends Component {
     state = {
-        heroId: 99
+        heroId: 99,
+        imgURL: "https://i.pinimg.com/236x/9b/76/75/9b767505f5a5df3df348a898ba4ae8bb.jpg",
+        movies: ['movie 1', 'movie2', 'lol'],
+        likeCount: 0
     };
     render() {
         // return <h1>Avengers incoming...</h1> //'()' not required for single element
@@ -17,12 +20,50 @@ class Hero extends Component {
                 </button>
                 <br />
                 <p>{this.isHero()}</p>
+                <br />
+                <br />
+                <div>
+                    <div className="card" style={{ width: '18rem' }}>
+                        <img src={this.state.imgURL} className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">Avenger Name</h5>
+                            <p className="card-text"></p>
+                            <ul>
+                                {this.showMovies()}
+                                {/* {this.state.movies.map(movie => <li>{movie}</li>)} */}
+                            </ul>
+                            <button className="btn btn-primary" onClick={()=>{this.likeAvenger(2)}} >
+                                Like <span className="badge badge-light">                                    {this.state.likeCount}                                </span>
+                            </button> {/*[ this.likeAvenger() ]-calls method on render [ this.likeAvenger ] method reference */}
+                        </div>
+                    </div>
+                </div>
             </React.Fragment>
         );
     }
 
     isHero() {
         return (this.state.heroId > 0) ? "Is an Avenger" : "Not an Avenger";
+    }
+
+    showMovies() {
+        // return this.state.movies.map(movie => <li>{movie}</li>);
+        // this.state.movies = [];
+        if (this.state.movies.length === 0) return <p>No movies available</p>
+        return this.state.movies.map((movie, i) => <li key={i}>{movie}</li>);
+    }
+
+    // likeAvenger = () => {
+    //     this.setState({ likeCount: ++this.state.likeCount });
+    //     console.clear();
+    //     console.log("Like Count: " + this.state.likeCount);
+    // }
+
+
+    likeAvenger = (likeIncrementStep) => {
+        this.setState({ likeCount: this.state.likeCount + likeIncrementStep });
+        console.clear();
+        console.log("Like Count: " + this.state.likeCount);
     }
 }
 
