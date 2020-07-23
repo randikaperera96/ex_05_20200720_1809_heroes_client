45d5a3be6cff5defe25f3eea0e5aec69d196257f
randikaperera96 <randikaperera135@gmail.com>
2020-07-23 18:11:40 +0530

    Demo react "function" and "inline CSS"

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -9,10 +9,20 @@ class Hero extends Component {
         // return <div><h1>Avengers incoming...</h1><button>Click Here</button></div>  //'<React.Fragment></React.Fragment>' Or '<></>' can be used instead of <div></div>
         return (
             <React.Fragment>
-                <h1>Avengers incoming...</h1>
-                <button>Click Here {this.state.heroId}</button>
+                {/* <h1 style="width:'18px'">Avengers incoming...</h1> // when using inline CSS we should use JS obects instesd of plain CSS */}
+                <h1 style={{ width: "18rem", fontSize: "90px" }}>Avengers incoming...</h1>
+                {/* <button>Click Here {this.state.heroId}</button> */}
+                <button type="button" className="btn btn-primary">
+                    Click on Avenger
+                </button>
+                <br />
+                <p>{this.isHero()}</p>
             </React.Fragment>
         );
+    }
+
+    isHero() {
+        return (this.state.heroId > 0) ? "Is an Avenger" : "Not an Avenger";
     }
 }
 
